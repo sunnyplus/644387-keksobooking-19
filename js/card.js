42b1e37041bb35756d7c508da49cfaf85bd50d56
alexey <sunnyplus@mail.ru>
2020-02-28 21:07:29 +0300

    6-2

--- js/card.js
+++ js/card.js
@@ -26,22 +26,22 @@
     var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
     var newCard = cardTemplate.cloneNode(true);
     newCard.querySelector('.popup__avatar').src = similarAd.author.avatar;
-    newCard.querySelector('.popup__title').textContent = similarAd.author.offer.title;
-    newCard.querySelector('.popup__text--address').textContent = similarAd.author.offer.address;
-    newCard.querySelector('.popup__text--price').textContent = similarAd.author.offer.price + ' ₽';
+    newCard.querySelector('.popup__title').textContent = similarAd.offer.title;
+    newCard.querySelector('.popup__text--address').textContent = similarAd.offer.address;
+    newCard.querySelector('.popup__text--price').textContent = similarAd.offer.price + ' ₽';
     newCard.querySelector('.popup__text--price').append(document.createElement('span'));
     newCard.querySelector('span').textContent = '/ночь';
-    newCard.querySelector('.popup__type').textContent = offerTypes[similarAd.author.offer.type];
-    newCard.querySelector('.popup__text--capacity').textContent = similarAd.author.offer.rooms + ' комнаты для ' + similarAd.author.offer.guests + ' гостей';
-    newCard.querySelector('.popup__text--time').textContent = 'заезд после ' + similarAd.author.offer.checkin + ', выезд до ' + similarAd.author.offer.checkout;
+    newCard.querySelector('.popup__type').textContent = offerTypes[similarAd.offer.type];
+    newCard.querySelector('.popup__text--capacity').textContent = similarAd.offer.rooms + ' комнаты для ' + similarAd.offer.guests + ' гостей';
+    newCard.querySelector('.popup__text--time').textContent = 'заезд после ' + similarAd.offer.checkin + ', выезд до ' + similarAd.offer.checkout;
     newCard.querySelector('.popup__features').remove(); // фичи
-    var featuresList = createNode('ul', 'popup__features', similarAd.author.offer.features, 'li', 'popup__feature');
+    var featuresList = createNode('ul', 'popup__features', similarAd.offer.features, 'li', 'popup__feature');
     newCard.querySelector('.popup__description').before(featuresList);
-    newCard.querySelector('.popup__description').textContent = similarAd.author.offer.description;
+    newCard.querySelector('.popup__description').textContent = similarAd.offer.description;
 
-    for (var i = 0; i < similarAd.author.offer.photos.length; i++) {
+    for (var i = 0; i < similarAd.offer.photos.length; i++) {
       var photoTemplate = newCard.querySelector('.popup__photo').cloneNode(true);
-      photoTemplate.src = similarAd.author.offer.photos[i];
+      photoTemplate.src = similarAd.offer.photos[i];
       var photoPopups = newCard.querySelector('.popup__photos');
       photoPopups.append(photoTemplate);
     }
--- js/page.js
+++ js/page.js
@@ -9,13 +9,25 @@
     });
   };
 
+  var onTryAgainButtonPress = function (evt) {
+    evt.preventDefault();
+    document.querySelector('.error').remove();
+    pageDeactivate(true);
+    document.querySelector('.ad-form').classList.add('ad-form--disabled');
+    document.querySelector('.map').classList.add('map--faded');
+  };
+
   var pageActivate = function () {
     pageDeactivate(false);
     window.load('https://js.dump.academy/keksobooking/data', function (similarAds) {
-
       window.map.drawSimilarAds(similarAds);
-    }, function () {
-      // непонятно что делать с ошибками
+    }, function (error) {
+      var errorFragment = document.createDocumentFragment();
+      var errorPopup = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
+      errorPopup.querySelector('.error__message').textContent = error;
+      errorFragment.append(errorPopup);
+      document.body.append(errorFragment);
+      errorPopup.querySelector('.error__button').addEventListener('click', onTryAgainButtonPress);
     });
     window.form.checkCapacityValidity(); // проверка на валидность поля capacity (кол-во гостей)
     document.querySelector('.ad-form').classList.remove('ad-form--disabled');
