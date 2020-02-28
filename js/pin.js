f977235f2690879428439ff9aea10ae3d7031965
alexey <sunnyplus@mail.ru>
2020-02-28 19:40:46 +0300

    6-2

--- js/pin.js
+++ js/pin.js
@@ -4,11 +4,11 @@
 
   var renderSimilarAds = function (similarAd) { // функция генерации пинов, на входе pin - данные по одному объявлению
     var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin').cloneNode(true);
-    var offsetX = similarAd.author.location.x - pinTemplate.offsetWidth / 2;
-    var offsetY = similarAd.author.location.y - pinTemplate.offsetHeight;
+    var offsetX = similarAd.location.x - pinTemplate.offsetWidth / 2;
+    var offsetY = similarAd.location.y - pinTemplate.offsetHeight;
     pinTemplate.style = 'left: ' + offsetX + 'px; top: ' + offsetY + 'px';
     pinTemplate.querySelector('img').src = similarAd.author.avatar;
-    pinTemplate.querySelector('img').alt = similarAd.author.offer.title;
+    pinTemplate.querySelector('img').alt = similarAd.offer.title;
 
     return pinTemplate;
   };
