f70d87614797798722ac451bda115de81e42b82c
Alexey <35955526+sunnyplus@users.noreply.github.com>
2020-03-14 22:59:14 +0300

    фильтрация по всем параметрам

--- js/filter.js
+++ js/filter.js
@@ -25,38 +25,41 @@
   
   var onFiltersFormChange = function (evt) {
 
-  if (document.querySelector('.map__card')) {
-    document.querySelector('.map__card').remove();
-  }
-    switch (evt.target.name) {
-      case 'features':
-        // console.log('это фичи');
-        break;
-      case 'housing-price':
-		// filteredData = filteredData.filter(function (it) {
-		// 	return it.offer.price > housingPrices[evt.target.value]['min'];
-		// });
-		reDrawSimilarAds(filteredData);
-        break;
-      case 'housing-rooms':
-        // console.log('изменилось кол-во комнат');
-        break;
-      case 'housing-type':
-        if (evt.target.value !== 'any') {
-          filteredData = window.backend.similarAds.slice().filter(function (it, i) {
-            return it.offer.type === evt.target.value;
-            });
-        } else {
-          filteredData = window.backend.similarAds.slice();
-        }
-        filteredData = amount(filteredData, 5);
-		    reDrawSimilarAds(filteredData);
-        break;
-      case 'housing-guests':
-        // console.log('изменилось кол-во гостей');
-        break;
-      default:
-        throw new Error('Неизвестный параметр ' + evt.target.name);
+    var fd = new FormData(filtersForm);
+    filteredData = window.backend.similarAds.slice();
+
+    if (fd.getAll('features').length > 0) {
+      fd.getAll('features').forEach(function (element) {
+        filteredData = filteredData.filter(function (it) {
+          return it.offer.features.indexOf(element) !== -1;
+        });
+      });
+    }
+    if (fd.getAll('housing-type')[0] !== 'any') {
+      filteredData = filteredData.filter(function (it) {
+        return it.offer.type === fd.getAll('housing-type')[0];
+      });
+    }
+    if (fd.getAll('housing-rooms')[0] !== 'any') {
+      filteredData = filteredData.filter(function (it) {
+        return it.offer.rooms === Number(fd.getAll('housing-rooms')[0]);
+      });
+    }
+    if (fd.getAll('housing-guests')[0] !== 'any') {
+      filteredData = filteredData.filter(function (it) {
+        return it.offer.guests === Number(fd.getAll('housing-guests')[0]);
+      });
+    }
+    if (fd.getAll('housing-price')[0] !== 'any') {
+      filteredData = filteredData.filter(function (it) {
+        return it.offer.price === housingPrices[fd.getAll('housing-price')[0]]['min']
+      });
+    }
+    console.log(filteredData);
+    reDrawSimilarAds(filteredData);
+
+    if (document.querySelector('.map__card')) {
+      document.querySelector('.map__card').remove();
     }
   };
   filtersForm.addEventListener('change', onFiltersFormChange);
